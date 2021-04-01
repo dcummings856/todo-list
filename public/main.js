457499feef836755ebaebad732d9a0d3a934b2a7
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-04-01 13:58:00 -0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,34 @@
+const update = document.querySelectorAll('.complete')
+const deleted = document.querySelectorAll('.delete')
+
+Array.from(update).forEach((element) => {
+  element.addEventListener('click', updateComplete)
+})
+
+Array.from(deleted).forEach((element) => {
+  element.addEventListener('click', removeTodo)
+})
+
+async function updateComplete(){
+  const todoName = this.parentNode.innerText
+  console.log(todoName)
+
+  try{
+    const response = await fetch('todos', {
+      method: 'put', 
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'todosX': todoName
+      })
+    })
+    const data = await response.json()
+    console.log(data)
+    location.reload()
+  }catch(err){
+    console.log(err)
+  }
+}
+
+async function removeTodo(){
+  
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+const PORT = 8000
+require('dotenv').config()
+
+let dbConnectionStr = process.env.DB_STRING
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: 
+true })
+  .then(client => {
+  console.log('Connected to Database')
+  const db = client.db('todo-list-todos')
+  const todosCollection = db.collection('todos')
+  // const completedCollection = db.collection('completedTodos')
+
+  app.get('/', (request, response) => {
+    db.collection('todos').find().toArray()
+      .then(results => {
+        response.render('index.ejs', { todos: results })
+      })
+      .catch(error => console.error(error))
+  })
+  
+  app.post('/todos', (request, response) => {
+    todosCollection.insertOne(request.body)
+      .then(result => {
+        response.redirect('/')
+      })
+      .catch(error => console.error(error))
+  })
+
+  app.put('/todos', (request, response) => {
+    todosCollection.updateOne({todos: request.body.todosX}, {
+      $set: {
+        todos: request.body.todosX + 'completed'
+      }
+    })
+    .then(result => {
+      console.log('Completed')
+      response.json('JSON Completed')
+    })
+    .catch(error => console.error(error))
+  })
+  
+  app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+  })
+
+  })
+  .catch(error => console.log(error))
+
